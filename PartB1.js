151fece91cca2537d6143272aefdf81dc69d36e3
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-21 23:39:32 +1000

    Initial files for Part B1

---
+++ PartB1.js
@@ -0,0 +1,19 @@
+/**
+ * Part B1 - Data structures and algorithms - Simple data types
+ * JavaScript Assessment 2
+ */
+
+/**
+ * Repository created
+ * 
+ */
+
+/**
+ * --------------
+ * B1.2
+ * Build the array with following numbers
+ * [11, 5, 8, 3, 25, 16, 31, 45, 14, 20]
+ */
+
+const numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+console
