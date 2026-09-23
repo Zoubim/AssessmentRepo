e8a87ad166f0c621096490f68999df36d7adf651
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-23 10:11:21 +1000

    B2.2 questions with new js file created and B2.2 myMovie object included

---
+++ partB2.js
@@ -0,0 +1,24 @@
+/**
+ * Part B2 – Using basic data structures – Objects
+ * JavaScript Assessment 2
+ * Purpose: create an object, add properties to it, modify properties in it, and
+ * delete properties from it 
+ */
+
+
+/**
+ * ----------
+ * B2.2 - Build the object
+ * -----------
+ */
+B2.2 Build an object called “myMovie” with the following properties and data:
+•	Title: The longest day
+•	Year: 1964
+
+
+// myMovie object holding details of a single movie
+const myMovie = {
+    title: "The longest day",
+    year: 1964
+};
+console.log("B2.2 Object created:", myMovie)
